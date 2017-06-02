4ecfb4ed2627b13a56ea8d3db0eff6b959099775
raldenprog <raldenprog@mail.ru>
2017-06-02 20:48:31 +0000

    database

--- database.py
+++ database.py
@@ -1,11 +1,11 @@
 import sqlite3
 import uuid
-from check_parents_and_children import check
+from check_parents_and_children import check_login
 
 def create_table_parents():
     conn=sqlite3.connect('database.db')
     c = conn.cursor()
-    c.execute('''create table parents (id_parent int, id_child int, login varchar(255), password varchar(255), name varchar(255), surname varchar(255), patronymic varchar(255), sex varchar(255), number_parents int, number_close int, number_open int, number_needs int )''')
+    c.execute('''create table parents (id_parent int, id_child int, login varchar(255), password varchar(255), name varchar(255), surname varchar(255), patronymic varchar(255), sex varchar(255), number_parents int, number_close int, number_open int, number_needs int, balance_needs int, balance_close int, balance_open int )''')
     conn.commit()
     conn.close()
 #create_table_parents()
@@ -14,7 +14,7 @@ def create_table_parents():
 def create_table_children():
     conn=sqlite3.connect('database.db')
     c = conn.cursor()
-    c.execute('''create table children (id_child int, id_parent int, login varchar(255), password varchar(255), name varchar(255), surname varchar(255), patronymic varchar(255), sex varchar(255), number_close int, number_open int, number_needs int )''')
+    c.execute('''create table children (id_child int, id_parent int, login varchar(255), password varchar(255), name varchar(255), surname varchar(255), patronymic varchar(255), sex varchar(255), number_close int, number_open int, number_needs int, balance_needs int, balance_open )''')
     conn.commit()
     conn.close()
 #create_table_children()
@@ -46,7 +46,7 @@ def create_parent():
             "patronymic_child": "Egorovich",
             "sex_child": "man",
             }
-    answer_check=check(data)
+    answer_check=check_login(data)
     if answer_check!=0:
         print (answer_check["Answer"])
     else:
@@ -56,15 +56,15 @@ def create_parent():
         uid_child = str(uuid.uuid4())
         purchases = [(uid_parent, uid_child, data["login"], data["password"], data["name"], data["surname"], data["patronymic"], data["sex"], data["number_parents"], data["number_close"], data["number_open"], data["number_needs"])]
         #print (purchases)
-        c.executemany('INSERT INTO parents VALUES (?,?,?,?,?,?,?,?,?,?,?,?)', purchases)
+        c.executemany('INSERT INTO parents VALUES (?,?,?,?,?,?,?,?,?,?,?,?,0,0,0)', purchases)
         purchases = [(uid_child, uid_parent, data["login_child"], data["password_child"], data["name_child"], data["surname_child"], data[("patronymic_child")], data["sex_child"], data["number_close"], data["number_open"], data["number_needs"])]
-        c.executemany('INSERT INTO children VALUES (?,?,?,?,?,?,?,?,?,?,?)', purchases)
+        c.executemany('INSERT INTO children VALUES (?,?,?,?,?,?,?,?,?,?,?,0,0)', purchases)
         conn.commit()
         conn.close()
 #create_parent()
 
 def create_task():
-    data = {"id_parent": "df37b8fa-0ddb-4f72-a3c4-9ac13337ee3a",
+    data = {"id_parent": "118122ba-aac4-4dbc-b1da-37abbfc63bfe",
             "description": 'Kill heretics',
             "coin": 100500,
             }
@@ -81,7 +81,7 @@ def create_task():
     c.executemany('INSERT INTO tasks VALUES (?,?,?,?,?,?)', purchases)
     conn.commit()
     conn.close()
-create_task()
+#create_task()
 
 def get_table(table):
     con = sqlite3.connect("database.db")
