a7cf4b5f6186c924029a0e07e241bf519e489fdf
TXyZa <antonion2011@yandex.ru>
2017-06-03 22:56:29 +0700

    update

--- app/api/database/get_score_childrens.py
+++ app/api/database/get_score_childrens.py
@@ -1,18 +1,18 @@
 import json
 
 
-def average_score():
+def average_score(name_child):
     with open("score_children.json", 'r') as f:
-        data = f.read()
-        data = json.loads(data)
+        DATA = f.read()
+        DATA = json.loads(DATA)
     s = 0
-    score = []
-    for score in data:
-        if score != 'name':
-            s
-            s += float(data[score])
-
-    score = round(s/(len(data)-1), 1)
-    return data["name"], score
-
-print(average_score())
+    for data in DATA:
+        if data['name'] == name_child:
+            _score = {score: data[score] for score in data}
+            for score in data:
+                if score != 'name':
+                    s += float(data[score])
+            score = round(s/(len(data)-1), 1)
+            return _score, score
+    return None
+#print(average_score())
