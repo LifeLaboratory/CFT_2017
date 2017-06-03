248ef5a0f6a2d7e5b009cfe1e466822e1e0c1b3f
raldenprog <raldenprog@mail.ru>
2017-06-03 07:57:22 +0700

    database

--- database.py
+++ database.py
@@ -7,8 +7,7 @@ def create_table_parents():
     c = conn.cursor()
     c.execute('''create table parents (id_parent varchar(255), id_child varchar(255), login varchar(255), 
     password varchar(255), name varchar(255), surname varchar(255), patronymic varchar(255), 
-    sex varchar(255), number_parents int, number_close int, number_open int, number_needs int, 
-    balance_needs int, balance_close int, balance_open int )''')
+    sex varchar(255), number_parents int, balance_needs int, balance_close int, balance_open int )''')
     conn.commit()
     conn.close()
 #create_table_parents()
@@ -63,10 +62,9 @@ def create_parent():
         uid_parent = str(uuid.uuid4())
         uid_child = str(uuid.uuid4())
         purchases = [(uid_parent, uid_child, data["login"], data["password"], data["name"],
-                      data["surname"], data["patronymic"], data["sex"], data["number_parents"],
-                      data["number_close"], data["number_open"], data["number_needs"])]
+                      data["surname"], data["patronymic"], data["sex"], data["number_parents"])]
         #print (purchases)
-        c.executemany('INSERT INTO parents VALUES (?,?,?,?,?,?,?,?,?,?,?,?,0,0,0)', purchases)
+        c.executemany('INSERT INTO parents VALUES (?,?,?,?,?,?,?,?,?,0,0,0)', purchases)
         purchases = [(uid_child, uid_parent, data["login_child"], data["password_child"],
                       data["name_child"], data["surname_child"], data[("patronymic_child")],
                       data["sex_child"], data["number_close"], data["number_open"],
@@ -77,7 +75,7 @@ def create_parent():
 #create_parent()
 
 def create_task():
-    data = {"id_parent": "a38031fb-058a-428a-9dba-3efbdac070a9",
+    data = {"id_parent": "57095316-d6b9-4ffb-9a9d-754f558d0c57",
             "description": 'Kill heretics',
             "coin": 100500,
             }
@@ -95,7 +93,7 @@ def create_task():
     c.executemany('INSERT INTO tasks VALUES (?,?,?,?,?,?)', purchases)
     conn.commit()
     conn.close()
-#create_task()
+create_task()
 
 def get_table(table):
     con = sqlite3.connect("database.db")
@@ -105,6 +103,6 @@ def get_table(table):
     con.close
     return result
 
-#print(get_table('parents'))
-#print(get_table('children'))
-#print(get_table('tasks'))
+print(get_table('parents'))
+print(get_table('children'))
+print(get_table('tasks'))
