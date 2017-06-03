7f7cc20eeeea4ec1922e38145b2fbfb61b28e3ae
TXyZa <antonion2011@yandex.ru>
2017-06-03 17:07:45 +0700

    update login

--- app/api/database/login_manager.py
+++ app/api/database/login_manager.py
@@ -18,13 +18,13 @@ def login_in(login, password):
     conn, c = connect_db()
     sql = ("SELECT id_parent FROM parents where login = '{}' and password = '{}'".format(login, password))
     c.execute(sql)
-    result = c.fetchall()[0]
+    result = c.fetchall()
     print(result)
     if len(result) != 0:
         return {'id': result[0], 'status': 'parent'}
-    sql = ("SELECT id_children FROM children where login = '{}' and password = '{}'".format(login, password))
+    sql = ("SELECT id_child FROM children where login = '{}' and password = '{}'".format(login, password))
     c.execute(sql)
-    result = c.fetchall()[0]
+    result = c.fetchall()
     conn.close()
     if len(result) != 0:
         return {'id': result[0], 'status': 'children'}
